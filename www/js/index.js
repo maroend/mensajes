30a58d46c12f0a6afdd5faeb431cbd799041cba4
Charly <39631004+maroend@users.noreply.github.com>
2020-08-15 11:35:17 -0500

    fix

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,12 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+
+var pictureSource;   // picture source
+var destinationType; // sets the format of returned value 
+
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -34,6 +40,9 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+
+        pictureSource=navigator.camera.PictureSourceType;
+        destinationType=navigator.camera.DestinationType;
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -72,7 +81,7 @@ var app = {
 
       
 
-            
+
 
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
@@ -84,3 +93,89 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+
+
+// Called when a photo is successfully retrieved
+    //
+    function onPhotoDataSuccess(imageData) {
+        // Get image handle
+        //
+        var smallImage = document.getElementById('smallImage');
+  
+        // Unhide image elements
+        //
+        smallImage.style.display = 'block';
+  
+        // Show the captured photo
+        // The inline CSS rules are used to resize the image
+        //
+        smallImage.src = "data:image/jpeg;base64," + imageData;
+      }
+      
+      // Called when a photo is successfully retrieved
+      //
+      function onPhotoFileSuccess(imageData) {
+        // Get image handle
+        console.log(JSON.stringify(imageData));
+        
+           // Get image handle
+        //
+        var smallImage = document.getElementById('smallImage');
+  
+        // Unhide image elements
+        //
+        smallImage.style.display = 'block';
+  
+        // Show the captured photo
+        // The inline CSS rules are used to resize the image
+        //
+        smallImage.src = imageData;
+      }
+  
+      // Called when a photo is successfully retrieved
+      //
+      function onPhotoURISuccess(imageURI) {
+        // Uncomment to view the image file URI 
+        // console.log(imageURI);
+  
+        // Get image handle
+        //
+        var largeImage = document.getElementById('largeImage');
+  
+        // Unhide image elements
+        //
+        largeImage.style.display = 'block';
+  
+        // Show the captured photo
+        // The inline CSS rules are used to resize the image
+        //
+        largeImage.src = imageURI;
+      }
+  
+      // A button will call this function
+      //
+      function capturePhotoWithData() {
+        // Take picture using device camera and retrieve image as base64-encoded string
+        navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50 });
+      }
+  
+      function capturePhotoWithFile() {
+          navigator.camera.getPicture(onPhotoFileSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.FILE_URI });
+      }
+      
+      // A button will call this function
+      //
+      function getPhoto(source) {
+        // Retrieve image file location from specified source
+        navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
+          destinationType: destinationType.FILE_URI,
+          sourceType: source });
+      }
+  
+      // Called if something bad happens.
+      // 
+      function onFail(message) {
+        alert('Failed because: ' + message);
+      }
+  
